fd931c877b72a69358f88f8a00b12b38bfa66da2
rolme <roland.parnaso@gmail.com>
2013-05-04 17:22:26 -0700

    Initial find methods checkin.

--- oh-my-zsh/custom/plugins/rparnaso/rparnaso.plugin.zsh
+++ oh-my-zsh/custom/plugins/rparnaso/rparnaso.plugin.zsh
@@ -13,7 +13,7 @@ p () {
 _p () {
   for PROJECTS in $project_paths; do
     if [ -d "$PROJECTS" ]; then
-      _files -W $PROJECTS -/
+     _files -W $PROJECTS -/
     fi
   done
 }
@@ -32,6 +32,89 @@ dot-diff () {
 }
 
 ################################################################################
+# find methods
+################################################################################
+_find () {
+  local _verbose=0
+  local _dryrun=0
+  local _type=file
+  local _path=.
+  local _match=${@: -1}
+
+  while getopts :v:dt:p: OPTION; do
+    case $OPTION in
+      v) _verbose=1;;
+      d) _dryrun=1; _verbose=1;;
+      t) _type=$OPTARG;;
+      p) _path=$OPTARG;;
+    esac
+  done
+
+  [ "$_verbose" -eq 1 ] && printf "RUNNING: find %s -type %s -name %s" $_path $_type $_match
+  [ "$_dryrun" -eq 1 ]  || find $_path -type $_type -name $_match
+}
+
+findf () {
+  local _verbose=0
+  local _dryrun=0
+  local _path=.
+  local _match=${@: -1}
+
+  while getopts :h:v:dp: OPTION; do
+    case $OPTION in
+      h) echo "USAGE: findf [-hvd] [-p path=$_path] 'filename'"; return 0;;
+      v) _verbose=1;;
+      d) _dryrun=1; _verbose=1;;
+      p) _path=$OPTARG;;
+    esac
+  done
+
+  [ $_verbose -eq 1 ] && _find -d -p $_path $_match && echo
+  [ $_dryrun -eq 1 ]  || _find -p $_path $_match
+}
+
+findd () {
+  local _verbose=0
+  local _dryrun=0
+  local _path=.
+  local _match=${@: -1}
+
+  while getopts :h:v:dp: OPTION; do
+    case $OPTION in
+      h) echo "USAGE: findd [-hvd] [-p path=$_path] 'dirname'"; return 0;;
+      v) _verbose=1;;
+      d) _dryrun=1; _verbose=1;;
+      t) _type=$OPTARG;;
+      p) _path=$OPTARG;;
+    esac
+  done
+
+  [ $_verbose -eq 1 ] && _find -d -t directory -p $_path $_match && echo
+  [ $_dryrun -eq 1 ]  || _find -t directory -p $_path $_match
+}
+
+finds () {
+  local _verbose=0
+  local _dryrun=0
+  local _path=.
+  local _match=''
+  local _string=${@: -1}
+
+  while getopts :h:v:dp:f: OPTION; do
+    case $OPTION in
+      h) echo "USAGE: finds [-hvd] [-p path=$_path] [-f file-match] 'string'"; return 0;;
+      v) _verbose=1;;
+      d) _dryrun=1; _verbose=1;;
+      p) _path=$OPTARG;;
+      f) _match="${OPTARG}";;
+    esac
+  done
+
+  [ $_verbose -eq 1 ] && _find -d -t file -p $_path $_match && echo " | egrep -v 'tmp|git|log' | xargs grep '$_string'"
+  [ $_dryrun -eq 1 ]  || _find -t file -p $_path $_match | egrep -v 'tmp|git|log' | xargs grep $_string
+}
+
+################################################################################
 # ALIASES
 ################################################################################
 # ruby on rails
@@ -42,7 +125,7 @@ alias zc='vim ~/.zshrc'
 alias zo='vim ~/.oh-my-zsh'
 alias zp='vim ~/.oh-my-zsh/custom/plugins/rparnaso/rparnaso.plugin.zsh'
 alias zt='vim ~/.oh-my-zsh/themes/rparnaso.zsh-theme'
-alias zz='source ~/.zshrc'
+alias z='source ~/.zshrc'
 
 # shell commands
 alias psa='ps auxwww | grep'
